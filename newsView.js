ab035a16774837fd3e7353c35bd151757e8d68b8
Andy <ajscottlewis@gmail.com>
2023-01-08 13:48:12 +0000

    Draft constructor, write empty methods

--- newsView.js
+++ newsView.js
@@ -2,6 +2,29 @@ class NewsView {
   constructor(model, client) {
     this.model = model;
     this.client = client;
+    this.mainContainerEl = document.querySelector("#main-container");
+
+    this.searchInput = document.querySelector("#search-input").value;
+    this.searchButtonEl = document.querySelector("#search-btn");
+    this.searchButtonEl.addEventListener('click', () => {
+      this.searchHeadlines(searchInput);
+    });
+  }
+
+  displayHeadlines() {
+
+  }
+
+  displayHeadlinesFromAPI() {
+    
+  }
+
+  searchHeadlines(searchInput) {
+
+  }
+
+  displayError() {
+
   }
 }
 
